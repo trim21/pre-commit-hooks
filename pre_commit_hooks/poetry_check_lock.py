b3ff23888cbfe63d878a723aff5c7b87bbcd5bb4
Trim21 <i@trim21.me>
2020-10-03 22:18:36 +0800

    feat: add poetry-check-lock

---
+++ pre_commit_hooks/poetry_check_lock.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+import poetry.poetry
+import poetry.utils
+from poetry.factory import Factory
+
+
+def main() -> int:
+    if not current_repo().locker.is_fresh():
+        return 1
+    return 0
+
+
+def current_repo() -> poetry.poetry.Poetry:
+    return Factory().create_poetry(Path.cwd())
+
+
+if __name__ == "__main__":
+    exit(main())
